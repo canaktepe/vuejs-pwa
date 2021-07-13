277a84fedb7ecee00f09bbebf62a534e5e7b9875
Can AKTEPE <aktepecan@gmail.com>
2021-07-13 14:35:40 +0300

    fix

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,15 +1,5 @@
-self.addEventListener('message', (e) => {
-	if (!e.data) {
-		return;
-	}
-	switch (e.data) {
-		case 'skipWaiting':
-			self.skipWaiting();
-			break;
-		default:
-			break;
-	}
-});
-
-// The precaching code provided by Workbox.
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
+  }
+})
